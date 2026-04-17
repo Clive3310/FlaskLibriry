5df4e6b261b0f82a6bb8a8773c0e5d13832fbc27
timoha <mirafomushina7@gmail.com>
2026-04-17 15:38:36 +0300

    QuickFix

--- backend/run.py
+++ backend/run.py
@@ -18,4 +18,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80)
+    app.run(host='127.0.0.1', port=8080)
--- frontend/handlers/main_page.py
+++ frontend/handlers/main_page.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
-blue_main_page = Blueprint('front_page', __name__)
+blue_main = Blueprint('front_page', __name__)
 
 
-@blue_main_page.route('/')
+@blue_main.route('/')
 def index():
     return render_template("base.html")
